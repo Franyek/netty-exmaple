965f1b3ceccebf40a0a002580d2d8a08b6a7fbd9
franyek <franyek@gmail.com>
2016-05-19 08:00:03 +0200

    Handling wrong messages, collectiong penalty points

--- src/main/java/org/ch24/networking/game/server/ClientHandler.java
+++ src/main/java/org/ch24/networking/game/server/ClientHandler.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.net.SocketAddress;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,10 @@ public class ClientHandler extends SimpleChannelInboundHandler<String> {
     Gson gson = new Gson();
 
     public static final Map<SocketAddress, InputMessage> inputPackets = Collections.synchronizedMap(new HashMap<>());
+    //3 exception points and the team banned from the game
+    //TeamID-exception points
+//    private Map<Integer, Integer> exceptionNumbers = new HashMap<>();
+    private Map<SocketAddress, Integer> exceptionNumbers = new HashMap<>();
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
@@ -27,5 +32,25 @@ public class ClientHandler extends SimpleChannelInboundHandler<String> {
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
+    }
+
+    private void handleMsg(SocketAddress address, String msg){
+        //TODO Get team number by IP address and uses that number in the game
+        //String teamID
+        SocketAddress teamID = address;
+
+        int badPoints = 0;
+
+        if (exceptionNumbers.containsKey(teamID)) {
+            badPoints = this.exceptionNumbers.get(teamID);
+        }
+
+        this.exceptionNumbers.put(teamID, ++badPoints);
+
+        if (badPoints < 3) {
+            //TODO team is part of the game and msg should be handled
+        } else {
+            //TODO team out of the game
+        }
     }
 }
